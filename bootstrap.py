e4f76d8d99489b1ad609d42039e71d88c2d5dbf9
Jon Brule <brulejr@tgmail.com>
2024-11-09 23:28:30 -0500

    Initial version

---
+++ bootstrap.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+# Clone the repository
+subprocess.run(
+    ['git', 'clone', 'https://github.com/brulejr/ansible_homelab.git'])
+os.chdir('ansible_homelab')
+
+# Read user input
+server_ip = input("Enter server IP address: ")
+timezone = input("Enter timezone: ")
+username = input("Enter username: ")
+puid = input("Enter puid of the user: ")
+pgid = input("Enter pgid of the user: ")
+
+# Replace values in vars.yml file
+with open('group_vars/all/vars.yml', 'r') as f:
+    content = f.read()
+content = content.replace('<server_ip>', server_ip)
+content = content.replace('<timezone>', timezone)
+content = content.replace('<username>', username)
+content = content.replace('<puid>', puid)
+content = content.replace('<pgid>', pgid)
+with open('group_vars/all/vars.yml', 'w') as f:
+    f.write(content)
+
+# Replace values in inventory file
+with open('inventory', 'r') as f:
+    content = f.read()
+content = content.replace('<server_ip>', server_ip)
+content = content.replace('<username>', username)
+if use_password == 'y':
+    ssh_password = input("Enter SSH password: ")
+    content = content.replace(
+        'ansible_ssh_private_key_file = <path/to/private/key>', '')
+    content += f'ansible_ssh_pass = {ssh_password}\n'
+else:
+    private_key_path = input("Enter path to private key: ")
+    content = content.replace('<path/to/private/key>', private_key_path)
+with open('inventory', 'w') as f:
+    f.write(content)
+
+# Run the playbook
+subprocess.run(['ansible-playbook', 'main.yml'])
